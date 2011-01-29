d59d73a3a347dc78849734eabd09549211d2f3f4
Vincent Dieltiens <and1hotsauce@gmail.com>
2011-01-29 14:57:30 +0100

    - Documentation - Split in private functions - Lazy system

--- jquery.flextable.js
+++ jquery.flextable.js
@@ -1,8 +1,29 @@
+/**
+ * jQuery flextable plugin
+ * @name jquery-flextable.js
+ * @author Vincent Dieltiens - www.vincentdieltiens.be
+ * @version DEV-0.1
+ * @date January 29 2011
+ * @category jQuery plugin
+ * @copyright (c) 2011 Vincent Dieltiens (www.vincentdieltiens.com)
+ * @license CC Attribution-No Derivative Works 2.5 Brazil - http://creativecommons.org/licenses/by-nd/2.5/br/deed.en_US
+ * @example Visit http://www.vincentdieltiens.be/projects/jquery/flextable/ for more informations about this jQuery plugin
+ */
 (function($){
-	$.fn.flextable = function(options) {
+	/**
+	 * Create a flex table for each element
+	 * @param opts : the options defined by the user
+	 * @param lazy : only load needed
+	 */
+	$.fn.flextable = function(opts, lazy) {
 		// Extends and merges default options with options given by the user
-        var options = $.extend({}, $.fn.flextable.defaults, options);
-       
+		var options;
+		if( lazy == undefined ) {
+			options = $.extend({}, $.fn.flextable.defaults, opts);
+		} else {
+			options = $.extend({}, $.fn.flextable.lazyDefaults, opts);
+		}
+       	
         obj = this;
 
 		// Apply on each object
@@ -16,47 +37,67 @@
 			
 			var $table = $(this);
 			
-			// We add a <span> for each header to permit it to be resizable
-			$table.find('thead').find('th').each(function() {
-				$(this).prepend('<span class="ui-resize"></span>');
-			});
+			if( options.resizableClass != null ) {
+				set_header_resizeable($table, options);
+			}
 			
-			// For each td with the elipisis class, we wrap its content with
-			// a <div> which truncate the content if needed
-			$table.find('tbody').find('td.'+options.ellipsisClass).each(function() {
-				// Get the cell of the <td> cell
-				var cell_width = $(this).width();
-				
-				// Get the content of the <td> cell before wrapping
-				var cell_content = $(this).html();
-				
-				// Create the wrapper <div>
-				// Important : we need to set explicitly the width of the wrapper div
-				// 	to make the text-overflow: ellipsis working !	
-				var wrapper_div = $('<div />').html(cell_content).width(cell_width)
-					.css({
-						'white-space': 'nowrap',
-						'overflow': 'hidden',
-						'text-overflow': 'ellipsis',
-						'-o-text-overflow': 'ellipsis',
-						'-icab-text-overflow': 'ellipsis',
-						'-khtml-text-overflow': 'ellipsis',
-						'-moz-text-overflow': 'ellipsis',
-						'-webkit-text-overflow': 'ellipsis'
-					});
-				
-				// update the content of the <td> cell
-				$(this).html(wrapper_div);
-				
-				// put a title for the <td> cell
-				$(this).attr('title', cell_content);
-			});
-			
+			if( options.ellipsisClass != null ) {
+				ellipsis_cell_content($table, options);
+			}
         });
 	}
+	
+	function ellipsis_cell_content($table, options) {
+		// For each td with the elipisis class, we wrap its content with
+		// a <div> which truncate the content if needed
+		$table.find('tbody').find('td.'+options.ellipsisClass).each(function() {
+			
+			var $td = $(this);
+			
+			// Get the cell of the <td> cell
+			var cell_width = $td.width();
+			
+			// Get the content of the <td> cell before wrapping
+			var cell_content = $td.html();
+			
+			// Create the wrapper <div>
+			// Important : we need to set explicitly the width of the wrapper div
+			// 	to make the text-overflow: ellipsis working !	
+			var wrapper_div = $('<div />').html(cell_content).width(cell_width)
+				.css({
+					'white-space': 'nowrap',
+					'overflow': 'hidden',
+					'text-overflow': 'ellipsis',
+					'-o-text-overflow': 'ellipsis',
+					'-icab-text-overflow': 'ellipsis',
+					'-khtml-text-overflow': 'ellipsis',
+					'-moz-text-overflow': 'ellipsis',
+					'-webkit-text-overflow': 'ellipsis'
+				});
+			
+			// update the content of the <td> cell
+			$td.html(wrapper_div);
+			
+			// put a title for the <td> cell
+			$td.attr('title', cell_content);
+		});
+	}
    
+	function set_header_resizeable($table, options) {
+		// We add a <span> for each header to permit it to be resizable
+		$table.find('thead').find('th.'+options.resizeableClass).each(function() {
+			$(this).prepend('<span class="ui-resize"></span>');
+		});
+	}
+	
 	// Default options for this plugin
 	$.fn.flextable.defaults = {
-		ellipsisClass: 'truncate'
+		ellipsisClass: 'truncate',
+		resizeableClass: 'resizeable'
 	};
+	
+	$.fn.flextable.lazyDefaults = {
+		ellipsisClass: null,
+		resizeableClass: null,
+	}
 })(jQuery);
